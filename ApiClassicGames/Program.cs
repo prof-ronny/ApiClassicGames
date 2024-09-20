bbfc8853a904305ca940d5e8e1bb78d68c67a63b
prof-ronny <carlos.sousa34@fatec.sp.gov.br>
2024-09-20 09:38:39 -0300

    Migração Automatica

--- ApiClassicGames/Program.cs
+++ ApiClassicGames/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DbGamesContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
